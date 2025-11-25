cbff3386c4037203776cacca4670b739ce78e496
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-25 10:06:34 +0700

    add tabel ke database menggunakan sequelize

---
+++ models/komik.js
@@ -0,0 +1,29 @@
+module.exports=(sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id:{
+            type : DataTypes.INTEGER,
+            autoIncrement:true,
+            primaryKey: true
+        },
+        title:{ 
+            type: DataTypes.STRING,
+            allowNull:false,
+        },
+        description:{
+            type:DataTypes.STRING,
+            allowNull:false,    
+        },
+        author:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        imageType:DataTypes.STRING,
+        imageName:DataTypes.STRING,
+        ImageData:DataTypes.BLOB('long'),
+    },
+    {
+        tableName: "komik",
+    });
+    return Komik;
+
+}
