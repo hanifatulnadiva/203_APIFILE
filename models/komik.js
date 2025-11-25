30ed84bf66f3fb3129a8d3a6c1c0d843b3592fea
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-25 11:53:44 +0700

    update service

--- models/komik.js
+++ models/komik.js
@@ -11,7 +11,7 @@ module.exports=(sequelize, DataTypes) => {
         },
         description:{
             type:DataTypes.STRING,
-            allowNull:false,    
+               
         },
         author:{
             type:DataTypes.STRING,
